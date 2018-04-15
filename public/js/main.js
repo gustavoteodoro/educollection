232d378bd6ee02d7825dc993042b6bc851f65142
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2018-04-15 16:17:59 -0300

    feat: Adding external video option

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var addUnit = document.querySelector('.addUnit');
-    var addUnitForm = document.querySelector('.addUnitForm');
+    const addUnit = document.querySelector('.addUnit');
+    const addUnitForm = document.querySelector('.addUnitForm');
 
     if(addUnit){
         addUnit.addEventListener('click', (e)=>{
@@ -9,7 +9,39 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 
-    // var multiSlides = document.querySelector('.course-carousel-1');
+    const videoOrigin = document.getElementById('videoOrigin');
+
+    if(videoOrigin) {
+        
+        const dynSpan = document.querySelectorAll('.dynSpan');
+        const ytvmTitle = document.getElementById('ytvmTitle');
+        const mp4Title = document.getElementById('mp4Title');
+        const ytDescription = document.getElementById('ytDescription');
+        const vmDescription = document.getElementById('vmDescription');
+        const mp4Description = document.getElementById('mp4Description');
+        const videoSource = document.getElementById('videoSource');
+
+        videoOrigin.addEventListener("change", () => {
+            dynSpan.forEach(item => item.classList.remove('-active'));
+            const selected = videoOrigin.value;
+            switch(selected) {
+                case "yt":
+                    ytvmTitle.classList.add('-active');
+                    ytDescription.classList.add('-active');
+                    break;
+                case "vm":
+                    ytvmTitle.classList.add('-active');
+                    vmDescription.classList.add('-active');
+                    break;
+                default:
+                    mp4Title.classList.add('-active');
+                    mp4Description.classList.add('-active');
+            }
+            videoSource.classList.add('-active');
+        });
+    }
+
+    // const multiSlides = document.querySelector('.course-carousel-1');
 
     // lory(multiSlides, {
     //     infinite: 4,
